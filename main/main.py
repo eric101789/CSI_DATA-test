35112a10b4d1212edf125a749c987e6a2fef432b
eric101789 <eric10179@gmail.com>
2022-03-14 21:07:48 +0800

    03/14 Series一維資料輸出(目前問題為字串切分)

--- main/main.py
+++ main/main.py
@@ -9,5 +9,18 @@ csv_path = res_path / 'my-experiment-file.csv'
 # 讀取csv檔案
 csi_data = pd.read_csv(csv_path)
 # print(csi_data)
-data_column_10 = csi_data['CSI_DATA'][10:11]
-print(data_column_10)
+
+csi_data['CSI_DATA'] = pd.Series(csi_data['CSI_DATA'], dtype="string")  # 輸出資料型別為DataFrame(二維資料)
+csi_data_10 = csi_data['CSI_DATA'][10:11]  # 輸出資料型別為Series(一維資料)
+print(csi_data_10)
+csi_data_10 = csi_data_10.str.replace("[", "")
+csi_data_10 = csi_data_10.str.replace("]", "")
+split_data = csi_data_10.str.split()  # 將資料轉為字串並切分
+print(split_data)
+data = pd.Series(split_data)
+print(data)
+
+# 選取特定資料片段(第十列CSI_DATA)
+# data_column_10 = csi_data['CSI_DATA'][10:11]
+# print(data_column_10)
+
