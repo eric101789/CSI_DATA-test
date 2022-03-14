96f2e217062a181c16bd0365cd55be236ad2b018
eric101789 <eric10179@gmail.com>
2022-03-14 17:03:08 +0800

    03/14 print column 10 data

--- main/main.py
+++ main/main.py
@@ -8,4 +8,6 @@ csv_path = res_path / 'my-experiment-file.csv'
 
 # 讀取csv檔案
 csi_data = pd.read_csv(csv_path)
-
+# print(csi_data)
+data_column_10 = csi_data['CSI_DATA'][10:11]
+print(data_column_10)
