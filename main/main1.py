374d358662d7070656857e4640e460cb776fbe76
eric101789 <eric10179@gmail.com>
2022-03-21 20:27:01 +0800

    03/21 修改蚊子程式碼內容(可成功輸出資料為TXT格式檔案)

--- main/main1.py
+++ main/main1.py
@@ -17,7 +17,8 @@ with open(csv_path, newline='') as csvfile:
     # print("--------------------------------------------")
 
     # 將 CSI DATA 轉成字串
-    data_str = "".join(data_list[9][25])
+    data_str = "".join(data_list[9][25])  # 透過 join() 連接 string 來串接起來
+    data_str2 = "".join(data_list[10][25])
     # print(data_str)
     # print(type(data_str))
     # print(len(data_str))
@@ -25,7 +26,8 @@ with open(csv_path, newline='') as csvfile:
 
     # 將 String 轉成 Series
     data_frame = pd.Series(data_str)
-    # print(data_frame)
+    data_frame2 = pd.Series(data_str2)
+    # print(data_frame2)
     # print("--------------------------------------------")
 
     # 將 Series 轉成字串並分割
@@ -33,27 +35,75 @@ with open(csv_path, newline='') as csvfile:
     data_frame = data_frame.str.replace("]", "")
     data_split = data_frame.str.split()
 
+    data_frame2 = data_frame2.str.replace("[", "")
+    data_frame2 = data_frame2.str.replace("]", "")
+    data_split2 = data_frame2.str.split()
+
     data = pd.Series(data_split)
-    print(data)
-    print("--------------------------------------------")
+    data2 = pd.Series(data_split2)
+    # print(data)
+    # print(data2)
+    # print("--------------------------------------------")
 
     # 將轉換成字串的 data 放進 list 裡
     mylist = list(data)
     mylist = mylist[0]
-    print(mylist)
-    print("--------------------------------------------")
+    # print(mylist)
+    # print("--------------------------------------------")
+
+    mylist2 = list(data2)
+    mylist2 = mylist2[0]
+    # print(mylist2)
+    # print("--------------------------------------------")
 
     # 將 list 裡的值放到 array(陣列) 裡
     result = np.array(mylist)
-    print(len(result))
-    print("--------------------------------------------")
+    result2 = np.array(mylist2)
+    # print(result)
+    # print(len(result))
+    # print("--------------------------------------------")
 
     # 印出想要的位數
-    print(result[13:20])
-    print("--------------------------------------------")
+    a = result[0:12]
+    a2 = result2[0:12]
+    # print(a)
+    b = result[12:64]
+    b2 = result2[12:64]
+    # print(b)
+    c = result[64:66]
+    c2 = result2[64:66]
+    # print(c)
+    d = result[66:118]
+    d2 = result2[66:118]
+    # print(d)
+    e = result[118:128]
+    e2 = result2[118:128]
+    # print(e)
+    # print("--------------------------------------------")
     # 印出奇數位
-    print(result[::2])
-    print("--------------------------------------------")
+    # print(result[::2])
+    # print("--------------------------------------------")
     # 印出偶數位
-    print(result[1::2])
-    print("--------------------------------------------")
+    # print(result[1::2])
+    # print("--------------------------------------------")
+
+with open("output1.txt", "w") as csvfile:
+    csvfile.write(" ".join(a))
+    csvfile.write("\n")
+    csvfile.write(" ".join(a2))
+with open("output2.txt", "w") as csvfile:
+    csvfile.write(" ".join(b))
+    csvfile.write("\n")
+    csvfile.write(" ".join(b2))
+with open("output3.txt", "w") as csvfile:
+    csvfile.write(" ".join(c))
+    csvfile.write("\n")
+    csvfile.write(" ".join(c2))
+with open("output4.txt", "w") as csvfile:
+    csvfile.write(" ".join(d))
+    csvfile.write("\n")
+    csvfile.write(" ".join(d2))
+with open("output5.txt", "w") as csvfile:
+    csvfile.write(" ".join(e))
+    csvfile.write("\n")
+    csvfile.write(" ".join(e2))
